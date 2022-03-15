1f1fb0e3a8ac9286ae0e9b2d621ee26269745077
Dmitry Chitalov <35995629+DmitryChitalov@users.noreply.github.com>
2022-03-15 15:54:34 +0500

    !

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ app_static = StaticMiddleware(application,
                               static_root='staticfiles',
                               static_dirs=STATIC_DIRS)
 
-if __name__ == '__main__':
-    with make_server('', 8080, app_static) as httpd:
-        print("Запуск на порту 8080...")
-        httpd.serve_forever()
+
+with make_server('stepeight.herokuapp.com', 8080, app_static) as httpd:
+    print("Запуск на порту 8080...")
+    httpd.serve_forever()
