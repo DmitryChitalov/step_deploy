50057c55a6e25d7321b9c8a5499108466b77ea39
Dmitry Chitalov <35995629+DmitryChitalov@users.noreply.github.com>
2022-03-15 15:52:32 +0500

    !

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ app_static = StaticMiddleware(application,
                               static_root='staticfiles',
                               static_dirs=STATIC_DIRS)
 
-with make_server('', 8080, app_static) as httpd:
-    print("Запуск на порту 8080...")
-    httpd.serve_forever()
+if __name__ == '__main__':
+    with make_server('', 8080, app_static) as httpd:
+        print("Запуск на порту 8080...")
+        httpd.serve_forever()
